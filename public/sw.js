fa9adcad128ec576c5ef66333f9548b795b9cd61
raven <vorontsof@mail.ru>
2026-08-12 01:59:33 +0400

    Обновлён кэш до версии 2.1.0

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "inventory-v2.0.1";
+const CACHE_NAME = "inventory-v2.1.0";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
 
 // Установка: кэшируем остов приложения.
